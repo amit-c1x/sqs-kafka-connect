87dce7310bbd564b03c573a8b4bd61c26f11ac0b
Derek Sayeau <derek.sayeau@qlik.com>
2017-05-31 11:51:04 -0400

    Update aws sdk versions (#1)
    * Update AWS SDK versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val Avro = "1.7.7"
-  val AwsSdk = "1.9.6"
-  val AwsSqsJms = "1.0.0"
+  val AwsSdk = "1.11.22"
+  val AwsSqsJms = "1.0.1"
   val Kafka = "0.10.0.1"
   val CommonLogging = "1.2"
   val Confluent = "3.0.0"
